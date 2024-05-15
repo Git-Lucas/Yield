e48e3c9c3912b70f69da956615c8945baf416322
Lucas Gonçalves <lucas.goncalves@forpeople.io>
2024-05-15 14:35:07 -0300

    Add project files.

---
+++ Yield/Program.cs
@@ -0,0 +1,23 @@
+﻿GetListInts().ToList().ForEach(Console.WriteLine);
+Console.WriteLine("--------------------------------------");
+GetYieldInts().ToList().ForEach(Console.WriteLine);
+
+IEnumerable<int> GetListInts()
+{
+    List<int> ints = [];
+
+    for (int i = 0; i < 5; i++)
+    {
+        ints.Add(i);
+    }
+
+    return ints;
+}
+
+IEnumerable<int> GetYieldInts()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        yield return i;
+    }
+}
